58cafa7d9c933c5ef4f6c6abece17ea6068eaece
mounirmkhal <mounir.mkhallati@gmail.com>
2024-09-14 12:06:00 -0700

    Add ws.close() to the websocket examples

--- script_examples/websockets_api_example_ws_images.py
+++ script_examples/websockets_api_example_ws_images.py
@@ -1,5 +1,5 @@
 #This is an example that uses the websockets api and the SaveImageWebsocket node to get images directly without
-#them being saved to disk
+#them being saved to disk.
 
 import websocket #NOTE: websocket-client (https://github.com/websocket-client/websocket-client)
 import uuid
