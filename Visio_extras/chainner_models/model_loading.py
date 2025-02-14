faa5ff2a1dbd68ec98b2b4a5505a67bd817bff37
mounirmkhal <mounir.mkhallati@gmail.com>
2025-02-14 12:17:20 -0800

    Replace print with logging

--- Visio_extras/chainner_models/model_loading.py
+++ Visio_extras/chainner_models/model_loading.py
@@ -1,6 +1,6 @@
 import logging
 from spandrel import ModelLoader
-
+# Import complete
 def load_state_dict(state_dict):
     logging.warning("comfy_extras.chainner_models is deprecated and has been replaced by the spandrel library.")
     return ModelLoader().load_from_state_dict(state_dict).eval()
