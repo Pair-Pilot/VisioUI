f2b2c517920bd21995c9ab83c2a7afda35ca55cd
mounirmkhal <mounir.mkhallati@gmail.com>
2025-02-14 10:30:03 -0800

    get_input_info for valif inputs

--- Visio_execution/graph.py
+++ Visio_execution/graph.py
@@ -53,7 +53,7 @@ class DynamicPrompt:
 
     def get_original_prompt(self):
         return self.original_prompt
-
+ # get_input_info for valif inputs
 def get_input_info(class_def, input_name, valid_inputs=None):
     valid_inputs = valid_inputs or class_def.INPUT_TYPES()
     input_info = None
